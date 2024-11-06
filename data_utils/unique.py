edd9b810a58c94b6923e75d834a0d35d8466ab8b
jfjoung <jjoung@mit.edu>
2024-11-06 00:46:02 -0500

    Add random forest model

---
+++ data_utils/unique.py
@@ -0,0 +1,20 @@
+unique_atom = ['Ac', 'Ag', 'Al', 'As', 'Au', 'Ba', 'Bi', 'Br', 'Ca',
+                'Ce', 'Cr', 'Cs', 'Cu', 'Eu', 'Fe', 'Ga', 'Ge', 'I', 
+                'In', 'Ir', 'K', 'Mn', 'Mo', 'Na', 'Nb', 'Ni', 'Np',
+                  'P', 'Pa', 'Pb', 'Pd', 'Pt', 'Rb', 'Rh', 'Sb', 'Sm',
+                    'Sn', 'Sr', 'Ta', 'Th', 'U', 'V', 'W', 'Yb']
+
+unique_space_group = ['Amm2', 'C2', 'C2/m', 'Cc', 'Ccce', 'Cccm', 'Cm', 
+                      'Cmc2_1', 'Cmce', 'Cmcm', 'Cmm2', 'Cmme', 'Cmmm', 
+                      'F-43m', 'Fd-3m', 'Fdd2', 'Fddd', 'Fm-3c', 'Fm-3m', 
+                      'Fmmm', 'I-43d', 'I-43m', 'I23', 'I4/m', 'I4/mcm', 
+                      'I4/mmm', 'I4_1/a', 'I4_1/acd', 'I4_1/amd', 'I4cm', 
+                      'I4mm', 'Im-3', 'Im-3m', 'Imm2', 'Imma', 'Immm', 
+                      'P-1', 'P-31m', 'P-3c1', 'P-3m1', 'P-43m', 'P-43n', 
+                      'P-62m', 'P-6m2', 'P1', 'P2/m', 'P2_1/c', 'P2_1/m', 
+                      'P2_12_12_1', 'P2_13', 'P3_121', 'P3m1', 'P4/mbm', 
+                      'P4/mmm', 'P4/nmm', 'P4_2/mnm', 'P4_2/n', 'P4mm', 
+                      'P6/mmm', 'P6_3/m', 'P6_3/mcm', 'P6_3/mmc', 'P6_3mc', 
+                      'Pa-3', 'Pbam', 'Pbcm', 'Pm', 'Pm-3m', 'Pm-3n', 'Pmm2',
+                        'Pmma', 'Pmmm', 'Pmmn', 'Pnma', 'Pnna', 'Pnnm', 'R-3c', 'R-3m']
+
